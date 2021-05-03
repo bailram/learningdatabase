cc46424a368c87f34285748e32c85e4a64c0b372
bailram <>
2021-05-03 13:25:12 +0700

    task 1

--- hotel.sql
+++ hotel.sql
@@ -1,6 +1,32 @@
-# Database
-### Task 1
-```sql
+-- phpMyAdmin SQL Dump
+-- version 5.0.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: May 03, 2021 at 08:22 AM
+-- Server version: 10.4.17-MariaDB
+-- PHP Version: 8.0.2
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `hotel`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `customer`
+--
+
 CREATE TABLE `customer` (
   `id` int(4) NOT NULL,
   `id_card_number` varchar(16) DEFAULT NULL,
@@ -11,6 +37,12 @@ CREATE TABLE `customer` (
   `address` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `price_room`
+--
+
 CREATE TABLE `price_room` (
   `id` int(4) NOT NULL,
   `room_type_id` int(4) NOT NULL,
@@ -19,6 +51,12 @@ CREATE TABLE `price_room` (
   `updated_by` varchar(15) DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `room_category`
+--
 
 CREATE TABLE `room_category` (
   `id` int(4) NOT NULL,
@@ -29,6 +67,12 @@ CREATE TABLE `room_category` (
   `created_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `room_type`
+--
+
 CREATE TABLE `room_type` (
   `id` int(4) NOT NULL,
   `room_category_id` int(4) NOT NULL,
@@ -38,6 +82,12 @@ CREATE TABLE `room_type` (
   `created_by` varchar(15) DEFAULT NULL,
   `created_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `stock_room`
+--
 
 CREATE TABLE `stock_room` (
   `id` int(4) NOT NULL,
@@ -51,6 +101,12 @@ CREATE TABLE `stock_room` (
   `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `transaction_history`
+--
+
 CREATE TABLE `transaction_history` (
   `id` int(4) NOT NULL,
   `number` varchar(15) DEFAULT NULL,
@@ -58,6 +114,12 @@ CREATE TABLE `transaction_history` (
   `created_by` varchar(15) DEFAULT NULL,
   `created_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `transaction_history_detail`
+--
 
 CREATE TABLE `transaction_history_detail` (
   `id` int(4) NOT NULL,
@@ -72,47 +134,94 @@ CREATE TABLE `transaction_history_detail` (
   `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `customer`
+--
 ALTER TABLE `customer`
   ADD PRIMARY KEY (`id`);
 
+--
+-- Indexes for table `price_room`
+--
 ALTER TABLE `price_room`
   ADD PRIMARY KEY (`id`),
   ADD KEY `price_room_fk` (`room_type_id`);
 
+--
+-- Indexes for table `room_category`
+--
 ALTER TABLE `room_category`
   ADD PRIMARY KEY (`id`);
 
+--
+-- Indexes for table `room_type`
+--
 ALTER TABLE `room_type`
   ADD PRIMARY KEY (`id`),
   ADD KEY `room_type_fk` (`room_category_id`);
 
+--
+-- Indexes for table `stock_room`
+--
 ALTER TABLE `stock_room`
   ADD PRIMARY KEY (`id`),
   ADD KEY `stock_room_fk` (`room_type_id`);
 
+--
+-- Indexes for table `transaction_history`
+--
 ALTER TABLE `transaction_history`
   ADD PRIMARY KEY (`id`),
   ADD KEY `transaction_history_fk` (`customer_id`);
 
+--
+-- Indexes for table `transaction_history_detail`
+--
 ALTER TABLE `transaction_history_detail`
   ADD PRIMARY KEY (`id`),
   ADD KEY `transaction_history_detail_fk` (`transaction_history_id`),
   ADD KEY `transaction_history_detail_fk_1` (`room_type_id`);
 
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `price_room`
+--
 ALTER TABLE `price_room`
   ADD CONSTRAINT `price_room_fk` FOREIGN KEY (`room_type_id`) REFERENCES `room_type` (`id`);
 
+--
+-- Constraints for table `room_type`
+--
 ALTER TABLE `room_type`
   ADD CONSTRAINT `room_type_fk` FOREIGN KEY (`room_category_id`) REFERENCES `room_category` (`id`);
 
+--
+-- Constraints for table `stock_room`
+--
 ALTER TABLE `stock_room`
   ADD CONSTRAINT `stock_room_fk` FOREIGN KEY (`room_type_id`) REFERENCES `room_type` (`id`);
 
+--
+-- Constraints for table `transaction_history`
+--
 ALTER TABLE `transaction_history`
   ADD CONSTRAINT `transaction_history_fk` FOREIGN KEY (`customer_id`) REFERENCES `customer` (`id`);
 
+--
+-- Constraints for table `transaction_history_detail`
+--
 ALTER TABLE `transaction_history_detail`
   ADD CONSTRAINT `transaction_history_detail_fk` FOREIGN KEY (`transaction_history_id`) REFERENCES `transaction_history` (`id`),
   ADD CONSTRAINT `transaction_history_detail_fk_1` FOREIGN KEY (`room_type_id`) REFERENCES `room_type` (`id`);
 COMMIT;
-```
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
